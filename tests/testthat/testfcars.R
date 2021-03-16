f7e4d849e7a8b6a6241e6924cae2b5e2bc180685
gben1750 <gbenson21a@gmail.com>
2021-03-16 18:45:03 +1100

    Commit changes to test script - add another test..looking for first build from Travis.

--- tests/testthat/testfcars.R
+++ tests/testthat/testfcars.R
@@ -9,3 +9,9 @@ test_that("Test number of records in 2013 dataset should equal 30202", {
   tbl <- fars_read(fileName)
   expect_equal(nrow(tbl), 30202)
 })
+
+test_that("Test number of records in 2013 dataset should equal 30202", {
+  fileName = 'accident_2013_test.csv'
+  tbl <- fars_read(fileName)
+  expect_equal(nrow(tbl), 30202)
+})
